cfdd8b97d065eb776623adeab57babca15ad88f4
cfos3120 <cfos3120@uni.sydney.edu>
2024-07-29 14:20:58 +1000

    fix

--- main_step.py
+++ main_step.py
@@ -174,9 +174,9 @@ if __name__ == '__main__':
 
     # Override for Step Case
     #dataset_args['name'] = 'Step'
-    dataset_args['file_path'] = r'C:\Users\Noahc\Documents\USYD\tutorial\python_utils\backward_facing_step_normalized.npy'
-    dataset_args['file_path'] = r'C:\Users\Noahc\Documents\USYD\tutorial\python_utils\cavity_with_bc_normalized.npy'
-    dataset_args['sub_x'] = 0.001
+    # dataset_args['file_path'] = r'C:\Users\Noahc\Documents\USYD\tutorial\python_utils\backward_facing_step_normalized.npy'
+    # dataset_args['file_path'] = r'C:\Users\Noahc\Documents\USYD\tutorial\python_utils\cavity_with_bc_normalized.npy'
+    # dataset_args['sub_x'] = 0.001
     # dataset_args['bc_input_f'] = False
 
     # 1. Dataset Preperations:
